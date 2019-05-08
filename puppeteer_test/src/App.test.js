16f697361ea9c66572312f8deb3f1953bce7bbbb
Luke Mico <lukemico@me.com>
2019-05-08 23:30:44 +1000

    Setup and Install Dependencies for Testing with Google's Puppeteer

--- puppeteer_test/src/App.test.js
+++ puppeteer_test/src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const puppeteer = required("puppeteer");
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const isDebugging = () => {
+	const debugging_mode = {
+		headless: false,
+		slowMo: 250,
+		devTools: true
+	};
+	return process.env.node_ENV === "debug" ? debugging_mode : {};
+};
+
+describe("on page load", () => {
+	TestRunner("h1 loads correctly", async () => {
+		let browser = await puppeteer.launch({});
+		let page = await browser.newPage();
+
+		page.emulate({
+			viewport: {
+				width: 500,
+				height: 2400
+			},
+			userAgent: ""
+		});
+	});
 });
